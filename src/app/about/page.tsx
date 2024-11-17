a37b428ffa3895594f1a3787e05225ef3c3553fd
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-11-16 20:33:36 -0500

    Add button to home using next router

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,16 @@
+"use client";
 import { useRouter } from "next/navigation";
 export default function About() {
   const router = useRouter();
-  return <h1>About Us</h1>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <button
+        onClick={() => router.push("/")}
+        className="bg-blue-500 text-white p-2 rounded-md"
+      >
+        Home
+      </button>
+    </div>
+  );
 }
